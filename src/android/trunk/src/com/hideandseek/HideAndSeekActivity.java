85cd3b3487d816ac2b0a9c5095d68c9cf08725d5
Paulo Fernando <pf@paulofernando.net.br>
2011-11-04 21:51:54 -0300

    Basic structure of the project

---
+++ src/android/trunk/src/com/hideandseek/HideAndSeekActivity.java
@@ -0,0 +1,13 @@
+package com.hideandseek;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HideAndSeekActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
